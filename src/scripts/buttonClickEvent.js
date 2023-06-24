eebd788cff8e4559f1f46e20b0a416c7e7da70fd
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-24 23:44:53 +0100

    Improved file structure. Division my zero results in a 'MATH ERROR' message which can be escaped by clicking any key.

--- src/scripts/buttonClickEvent.js
+++ src/scripts/buttonClickEvent.js
@@ -1,45 +1,16 @@
-const calculationStack = (string) => {
-  const calculation = string.split("");
-  calculation.pop();
-  return calculation.join("");
-};
+const controller = require("./calculatorController");
 
 const screen = document.querySelector("#screen");
 const buttonClickEvent = (symbol) => {
-  const operations = ["+", "-", "*", "/"];
   if (symbol === "=") {
-    // THIS IS GETTING CHUNKY
-    let calculationArray = screen.textContent
-      .split("")
-      .map((el) => {
-        return operations.includes(el) ? ` ${el} ` : el;
-      })
-      .join("")
-      .split(" ")
-      .map((el) => {
-        return operations.includes(el) ? el : el * 1;
-      });
-
-    let answer;
-    if (calculationArray[1] === "+") {
-      answer = calculationArray[0] + calculationArray[2];
-    } else if (calculationArray[1] === "-") {
-      answer = calculationArray[0] - calculationArray[2];
-    } else if (calculationArray[1] === "*") {
-      answer = calculationArray[0] * calculationArray[2];
-    } else if (calculationArray[1] === "/") {
-      answer =
-        calculation[2] === 0
-          ? "MATH ERROR"
-          : calculationArray[0] / calculationArray[2];
-    }
-
-    screen.textContent = answer;
-    console.log(calculationArray);
+    const calculationArray = controller.prepare(screen.textContent);
+    screen.textContent = controller.evaluator(calculationArray);
   } else if (symbol === "DEL") {
-    screen.textContent = calculationStack(screen.textContent);
+    screen.textContent = controller.stack(screen.textContent);
   } else if (symbol === "AC") {
     screen.textContent = "";
+  } else if (screen.textContent === "MATH ERROR") {
+    screen.textContent = symbol;
   } else {
     screen.textContent += symbol;
   }
