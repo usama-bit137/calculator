bb28d4aacf6e683db6e1086d62801dc5463896ae
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-24 17:30:57 +0100

    File structure changed and code made modular using ES6 imports.

--- main.js
+++ main.js
@@ -7,26 +7,100 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("// entry-point\r\n\r\nconst buttonArray = [...document.querySelectorAll(\".grid-item\")]; // node list.\r\n\r\nconst buttonClickEvent = (symbol) => {\r\n  console.log(symbol);\r\n};\r\n\r\nconst addEventListenerList = (list) => {\r\n  list.forEach((item) => {\r\n    item.addEventListener(\"click\", () => {\r\n      buttonClickEvent(item.textContent);\r\n    });\r\n  });\r\n};\r\n\r\naddEventListenerList(buttonArray);\r\n\n\n//# sourceURL=webpack://calculator/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scripts_addEventListenerList__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scripts/addEventListenerList */ \"./src/scripts/addEventListenerList.js\");\n\r\nconst buttonArray = [...document.querySelectorAll(\".grid-item\")]; // node list.\r\n\r\n(0,_scripts_addEventListenerList__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(buttonArray);\r\n\n\n//# sourceURL=webpack://calculator/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/scripts/addEventListenerList.js":
+/*!*********************************************!*\
+  !*** ./src/scripts/addEventListenerList.js ***!
+  \*********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _buttonClickEvent__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./buttonClickEvent */ \"./src/scripts/buttonClickEvent\");\n\r\n\r\nconst addEventListenerList = (list) => {\r\n  list.forEach((item) => {\r\n    item.addEventListener(\"click\", () => {\r\n      (0,_buttonClickEvent__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(item.textContent);\r\n    });\r\n  });\r\n};\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (addEventListenerList);\r\n\n\n//# sourceURL=webpack://calculator/./src/scripts/addEventListenerList.js?");
+
+/***/ }),
+
+/***/ "./src/scripts/buttonClickEvent":
+/*!**************************************!*\
+  !*** ./src/scripts/buttonClickEvent ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst screen = document.querySelector(\"#screen\");\r\nconst buttonClickEvent = (symbol) => {\r\n  screen.textContent = symbol;\r\n};\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (buttonClickEvent);\r\n\n\n//# sourceURL=webpack://calculator/./src/scripts/buttonClickEvent?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
