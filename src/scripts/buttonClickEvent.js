8dc3986596ce37ae1ecbe1db52a1ff079af692d8
usama-bit137 <usamaaqeel98@gmail.com>
2023-06-24 21:54:49 +0100

    Multidigit calculations working for all number lengths.

--- src/scripts/buttonClickEvent.js
+++ src/scripts/buttonClickEvent.js
@@ -1,31 +1,48 @@
-const calculationPrep = (string) => {
-  const operations = ["+", "-", "*", "/"];
-  const calculation = string.split(" ");
+const calculationStack = (string) => {
+  const calculation = string.split("");
   calculation.pop();
-  return operations.includes(calculation[-1])
-    ? calculation.join(" ") + " "
-    : calculation.join(" ");
+  return calculation.join("");
 };
 
 const screen = document.querySelector("#screen");
 const buttonClickEvent = (symbol) => {
   const operations = ["+", "-", "*", "/"];
-
   if (symbol === "=") {
-    screen.textContent = symbol;
-    // SPLIT INTO AN ARRAY AND PASS INTO A FUNCTION
-    return;
-  } else if (operations.includes(symbol)) {
-    screen.textContent += " " + symbol + " ";
-    return;
+    // THIS IS GETTING CHUNKY
+    let calculationArray = screen.textContent
+      .split("")
+      .map((el) => {
+        return operations.includes(el) ? ` ${el} ` : el;
+      })
+      .join("")
+      .split(" ")
+      .map((el) => {
+        return operations.includes(el) ? el : el * 1;
+      });
+
+    let answer;
+    if (calculationArray[1] === "+") {
+      answer = calculationArray[0] + calculationArray[2];
+    } else if (calculationArray[1] === "-") {
+      answer = calculationArray[0] - calculationArray[2];
+    } else if (calculationArray[1] === "*") {
+      answer = calculationArray[0] * calculationArray[2];
+    } else if (calculationArray[1] === "/") {
+      answer =
+        calculation[2] === 0
+          ? "MATH ERROR"
+          : calculationArray[0] / calculationArray[2];
+    }
+
+    screen.textContent = answer;
+    console.log(calculationArray);
   } else if (symbol === "DEL") {
-    screen.textContent = calculationPrep(screen.textContent);
-    return;
+    screen.textContent = calculationStack(screen.textContent);
   } else if (symbol === "AC") {
     screen.textContent = "";
-    return;
+  } else {
+    screen.textContent += symbol;
   }
-  screen.textContent += symbol;
 };
 
 export default buttonClickEvent;
